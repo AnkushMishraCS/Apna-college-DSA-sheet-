cd893509b5166a3c3d05344d497bac2297c6b3ca
Ankush Mishra <ankushmishra.candidate@gmail.com>
2025-03-19 20:00:44 +0530

    code1

--- code1.cpp
+++ code1.cpp
@@ -1,37 +1,29 @@
-  #include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main() {
- 
-    // sytax
+void findMaxMin(int arr[], int size, int &maximum, int &minimum) {
+    maximum = arr[0];
+    minimum = arr[0];
 
-// method 1
-    int arr[6]={1,2,5,32,44,22};
-// Method 2
-
-    int size=6;
-    int a[size];
-// Method #
-int crr[]={1,2,3,45,232};
-
-
-//  to print the array 
-
- for (int i=0;i<6;i++){
-    cout<<arr[i]<<" ";
- }
-cout<<endl;
-//   to take input of the array 
-
- int brr[6];
- for (int  i = 0; i < 6; i++)
- {
-  
-  cout<<"Enter the element "<<i<<" of the array ";
-   cin>>arr[i];
-
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > maximum) {
+            maximum = arr[i];
+        }
+        if (arr[i] < minimum) {
+            minimum = arr[i];
+        }
+    }
 }
- 
 
-return 0;
+int main() {
+    int numbers[] = {12, 5, 7, 19, 3, 21, 10};
+    int length = sizeof(numbers) / sizeof(numbers[0]);
+    int maximum, minimum;
+
+    findMaxMin(numbers, length, maximum, minimum);
+
+    cout << "Maximum Element: " << maximum << endl;
+    cout << "Minimum Element: " << minimum << endl;
+
+    return 0;
 }
